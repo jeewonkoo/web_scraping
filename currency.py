93a7a13120a8014c4e9f5a2cb8ff7e9820aeb5dc
jeewonkoo <47291707+jeewonkoo@users.noreply.github.com>
2020-02-09 14:27:37 -0800

    Add files via upload

---
+++ currency.py
@@ -0,0 +1,21 @@
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=1&ie=utf8&query=환율조회")
+
+c = r.content
+
+html = BeautifulSoup(c, "html.parser")
+
+div = html.find("div", {"class": "rate_tlt"})
+
+country = div.find("em", {"class": "t_nm"}).text
+
+print(country)
+
+rate = div.find("span", {"class": "spt_con up"}).text
+
+print(str(rate[3:]))
+
+
+
