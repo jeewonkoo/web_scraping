6fbf4e74cba4870987f2c85dfd52e2a257261659
jeewonkoo <47291707+jeewonkoo@users.noreply.github.com>
2020-02-11 00:23:06 -0800

    Update currency.py

--- currency.py
+++ currency.py
@@ -13,6 +13,9 @@ country = div.find("em", {"class": "t_nm"}).text
 
 print(country)
 
+### 전일대비 지수가 하락하자 함수 변경 "spt_con up" > "spt_con dw", 결과값으로 None 프린트 됌. 
+### 따라서 if else 함수를 사용하여 전일대비 지수 상승 혹은 하락에도 옳은 결과값이 프린트 되도록 수정
+
 rate_dw = div.find("span", {"class": "spt_con dw"})
 
 rate_up = div.find("span", {"class": "spt_con up"})
